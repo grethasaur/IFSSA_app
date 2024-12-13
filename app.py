94ad083976b0ef6ca9c682c67cc3d1d8be29d8b9
Gretha Pedro <144260376+grethasaur@users.noreply.github.com>
2024-12-12 21:59:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -633,11 +633,11 @@ def xai():
 
     # Display feature importance as a bar chart
     fig, ax = plt.subplots(figsize=(10, 6))  # Create a figure and axis
-    ax.bar(feature_importance_df['Feature'], feature_importance_df['Importance'])
-    ax.set_xlabel('Features')
-    ax.set_ylabel('Importance')
+    ax.bar(feature_importance_df['Importance'], feature_importance_df['Feature'])
+    ax.set_xlabel('Importance')
+    ax.set_ylabel('Features')
     ax.set_title('Feature Importance')
-    st.pyplot(fig)  # Explicitly pass the figure
+    st.pyplot(fig)
 
     # Residual Analysis
     st.header("Residual Analysis")
